b6a017a5870d31ea14029418d325c159b931be5d
RicharDev01 <ricardopineda587@gmail.com>
2022-08-10 15:13:03 -0600

    creacion y subida de disenio de app

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace CalculoCopago
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.in_precioNormal = new System.Windows.Forms.TextBox();
+      this.in_descuentoAcordado = new System.Windows.Forms.TextBox();
+      this.in_totalArancelado = new System.Windows.Forms.TextBox();
+      this.in_deducible = new System.Windows.Forms.TextBox();
+      this.in_porcentajeAprobado = new System.Windows.Forms.TextBox();
+      this.in_copago = new System.Windows.Forms.TextBox();
+      this.in_creditoSeguro = new System.Windows.Forms.TextBox();
+      this.txt_precioNormal = new System.Windows.Forms.Label();
+      this.txt_totalArancelado = new System.Windows.Forms.Label();
+      this.txt_deducible = new System.Windows.Forms.Label();
+      this.txt_descuentoAcordado = new System.Windows.Forms.Label();
+      this.txt_porentajeAprobado = new System.Windows.Forms.Label();
+      this.txt_copago = new System.Windows.Forms.Label();
+      this.txt_creditoSeguro = new System.Windows.Forms.Label();
+      this.txt_titulo = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // in_precioNormal
+      // 
+      this.in_precioNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_precioNormal.Location = new System.Drawing.Point(444, 167);
+      this.in_precioNormal.Name = "in_precioNormal";
+      this.in_precioNormal.Size = new System.Drawing.Size(233, 32);
+      this.in_precioNormal.TabIndex = 0;
+      this.in_precioNormal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      this.in_precioNormal.KeyUp += new System.Windows.Forms.KeyEventHandler(this.in_precioNormal_KeyUp);
+      // 
+      // in_descuentoAcordado
+      // 
+      this.in_descuentoAcordado.Enabled = false;
+      this.in_descuentoAcordado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_descuentoAcordado.Location = new System.Drawing.Point(443, 212);
+      this.in_descuentoAcordado.Name = "in_descuentoAcordado";
+      this.in_descuentoAcordado.Size = new System.Drawing.Size(233, 32);
+      this.in_descuentoAcordado.TabIndex = 1;
+      this.in_descuentoAcordado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // in_totalArancelado
+      // 
+      this.in_totalArancelado.Enabled = false;
+      this.in_totalArancelado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_totalArancelado.Location = new System.Drawing.Point(442, 260);
+      this.in_totalArancelado.Name = "in_totalArancelado";
+      this.in_totalArancelado.Size = new System.Drawing.Size(233, 32);
+      this.in_totalArancelado.TabIndex = 2;
+      this.in_totalArancelado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // in_deducible
+      // 
+      this.in_deducible.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_deducible.Location = new System.Drawing.Point(441, 307);
+      this.in_deducible.Name = "in_deducible";
+      this.in_deducible.Size = new System.Drawing.Size(233, 32);
+      this.in_deducible.TabIndex = 3;
+      this.in_deducible.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // in_porcentajeAprobado
+      // 
+      this.in_porcentajeAprobado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_porcentajeAprobado.Location = new System.Drawing.Point(440, 348);
+      this.in_porcentajeAprobado.Name = "in_porcentajeAprobado";
+      this.in_porcentajeAprobado.Size = new System.Drawing.Size(233, 32);
+      this.in_porcentajeAprobado.TabIndex = 4;
+      this.in_porcentajeAprobado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // in_copago
+      // 
+      this.in_copago.Enabled = false;
+      this.in_copago.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_copago.Location = new System.Drawing.Point(439, 391);
+      this.in_copago.Name = "in_copago";
+      this.in_copago.Size = new System.Drawing.Size(233, 32);
+      this.in_copago.TabIndex = 5;
+      this.in_copago.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // in_creditoSeguro
+      // 
+      this.in_creditoSeguro.Enabled = false;
+      this.in_creditoSeguro.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.in_creditoSeguro.Location = new System.Drawing.Point(438, 440);
+      this.in_creditoSeguro.Name = "in_creditoSeguro";
+      this.in_creditoSeguro.Size = new System.Drawing.Size(233, 32);
+      this.in_creditoSeguro.TabIndex = 6;
+      this.in_creditoSeguro.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      // 
+      // txt_precioNormal
+      // 
+      this.txt_precioNormal.AutoSize = true;
+      this.txt_precioNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_precioNormal.Location = new System.Drawing.Point(65, 167);
+      this.txt_precioNormal.Name = "txt_precioNormal";
+      this.txt_precioNormal.Size = new System.Drawing.Size(164, 26);
+      this.txt_precioNormal.TabIndex = 7;
+      this.txt_precioNormal.Text = "Precio Normal";
+      // 
+      // txt_totalArancelado
+      // 
+      this.txt_totalArancelado.AutoSize = true;
+      this.txt_totalArancelado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_totalArancelado.Location = new System.Drawing.Point(65, 260);
+      this.txt_totalArancelado.Name = "txt_totalArancelado";
+      this.txt_totalArancelado.Size = new System.Drawing.Size(191, 26);
+      this.txt_totalArancelado.TabIndex = 9;
+      this.txt_totalArancelado.Text = "Total Arancelado";
+      // 
+      // txt_deducible
+      // 
+      this.txt_deducible.AutoSize = true;
+      this.txt_deducible.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_deducible.Location = new System.Drawing.Point(65, 307);
+      this.txt_deducible.Name = "txt_deducible";
+      this.txt_deducible.Size = new System.Drawing.Size(118, 26);
+      this.txt_deducible.TabIndex = 10;
+      this.txt_deducible.Text = "Deducible";
+      // 
+      // txt_descuentoAcordado
+      // 
+      this.txt_descuentoAcordado.AutoSize = true;
+      this.txt_descuentoAcordado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_descuentoAcordado.Location = new System.Drawing.Point(65, 212);
+      this.txt_descuentoAcordado.Name = "txt_descuentoAcordado";
+      this.txt_descuentoAcordado.Size = new System.Drawing.Size(195, 26);
+      this.txt_descuentoAcordado.TabIndex = 11;
+      this.txt_descuentoAcordado.Text = "Descuento (10%)";
+      // 
+      // txt_porentajeAprobado
+      // 
+      this.txt_porentajeAprobado.AutoSize = true;
+      this.txt_porentajeAprobado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_porentajeAprobado.Location = new System.Drawing.Point(65, 348);
+      this.txt_porentajeAprobado.Name = "txt_porentajeAprobado";
+      this.txt_porentajeAprobado.Size = new System.Drawing.Size(248, 26);
+      this.txt_porentajeAprobado.TabIndex = 12;
+      this.txt_porentajeAprobado.Text = "Porcentaje Coaseguro";
+      // 
+      // txt_copago
+      // 
+      this.txt_copago.AutoSize = true;
+      this.txt_copago.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_copago.Location = new System.Drawing.Point(65, 391);
+      this.txt_copago.Name = "txt_copago";
+      this.txt_copago.Size = new System.Drawing.Size(94, 26);
+      this.txt_copago.TabIndex = 13;
+      this.txt_copago.Text = "Copago";
+      // 
+      // txt_creditoSeguro
+      // 
+      this.txt_creditoSeguro.AutoSize = true;
+      this.txt_creditoSeguro.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_creditoSeguro.Location = new System.Drawing.Point(65, 440);
+      this.txt_creditoSeguro.Name = "txt_creditoSeguro";
+      this.txt_creditoSeguro.Size = new System.Drawing.Size(226, 26);
+      this.txt_creditoSeguro.TabIndex = 14;
+      this.txt_creditoSeguro.Text = "Total a Aseguradora";
+      // 
+      // txt_titulo
+      // 
+      this.txt_titulo.AutoSize = true;
+      this.txt_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+      this.txt_titulo.Location = new System.Drawing.Point(203, 45);
+      this.txt_titulo.Name = "txt_titulo";
+      this.txt_titulo.Size = new System.Drawing.Size(371, 46);
+      this.txt_titulo.TabIndex = 15;
+      this.txt_titulo.Text = "Calculo de copago";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 524);
+      this.Controls.Add(this.txt_titulo);
+      this.Controls.Add(this.txt_creditoSeguro);
+      this.Controls.Add(this.txt_copago);
+      this.Controls.Add(this.txt_porentajeAprobado);
+      this.Controls.Add(this.txt_descuentoAcordado);
+      this.Controls.Add(this.txt_deducible);
+      this.Controls.Add(this.txt_totalArancelado);
+      this.Controls.Add(this.txt_precioNormal);
+      this.Controls.Add(this.in_creditoSeguro);
+      this.Controls.Add(this.in_copago);
+      this.Controls.Add(this.in_porcentajeAprobado);
+      this.Controls.Add(this.in_deducible);
+      this.Controls.Add(this.in_totalArancelado);
+      this.Controls.Add(this.in_descuentoAcordado);
+      this.Controls.Add(this.in_precioNormal);
+      this.Name = "Form1";
+      this.Text = " Calculo de Copago";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox in_precioNormal;
+    private System.Windows.Forms.TextBox in_descuentoAcordado;
+    private System.Windows.Forms.TextBox in_totalArancelado;
+    private System.Windows.Forms.TextBox in_deducible;
+    private System.Windows.Forms.TextBox in_porcentajeAprobado;
+    private System.Windows.Forms.TextBox in_copago;
+    private System.Windows.Forms.TextBox in_creditoSeguro;
+    private System.Windows.Forms.Label txt_precioNormal;
+    private System.Windows.Forms.Label txt_totalArancelado;
+    private System.Windows.Forms.Label txt_deducible;
+    private System.Windows.Forms.Label txt_descuentoAcordado;
+    private System.Windows.Forms.Label txt_porentajeAprobado;
+    private System.Windows.Forms.Label txt_copago;
+    private System.Windows.Forms.Label txt_creditoSeguro;
+    private System.Windows.Forms.Label txt_titulo;
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoCopago
+{
+  public partial class Form1 : Form
+  {
+
+    // Definicion de variables
+    float precioNormal;
+    float descuentoAcordado;
+    float precioArancel;
+    float deducible;
+    float porcentajeCoaseguro;
+    float copago;
+    float totalSeguro;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      precioNormal = 425.00F;
+      descuentoAcordado = 0.10F;
+      precioArancel = precioNormal - (precioNormal * descuentoAcordado);
+      deducible = 0;
+      porcentajeCoaseguro = 0.20F;
+      copago = precioArancel * porcentajeCoaseguro;
+      totalSeguro = precioArancel - copago;
+
+      in_precioNormal.Text = $"{precioNormal}";
+      in_descuentoAcordado.Text = $"{precioNormal*descuentoAcordado}";
+      in_totalArancelado.Text = $"{precioArancel}";
+      in_deducible.Text = $"{deducible}";
+      in_porcentajeAprobado.Text = $"{porcentajeCoaseguro}";
+      in_copago.Text = copago.ToString();
+      in_creditoSeguro.Text = $"{totalSeguro}";
+    }
+
+    private void in_precioNormal_KeyUp(object sender, KeyEventArgs e)
+    {
+      if(e.KeyCode == Keys.Enter)
+      {
+        precioNormal = float.Parse(in_precioNormal.Text);
+        descuentoAcordado = 0.10F;
+        precioArancel = precioNormal - (precioNormal * descuentoAcordado);
+        deducible = float.Parse(in_deducible.Text);
+        porcentajeCoaseguro = float.Parse(in_porcentajeAprobado.Text);
+        copago = precioArancel * porcentajeCoaseguro;
+        totalSeguro = precioArancel - copago;
+
+        in_precioNormal.Text = $"{precioNormal}";
+        in_descuentoAcordado.Text = $"{precioNormal * descuentoAcordado}";
+        in_totalArancelado.Text = $"{precioArancel}";
+        in_deducible.Text = $"$ {deducible}";
+        in_porcentajeAprobado.Text = $"{porcentajeCoaseguro}";
+        in_copago.Text = $"{copago}";
+        in_creditoSeguro.Text = $"{totalSeguro}";
+      }
+    }
+  }
+}
